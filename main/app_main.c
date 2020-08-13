d1a95b4dd7b6cb3911d38624d4cde98e1a5b9058
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2020-08-13 12:25:02 -0500

    Adding in barebones

--- main/app_main.c
+++ main/app_main.c
@@ -9,20 +9,55 @@
 #include "esp_flash_partitions.h"
 #include "esp_partition.h"
 #include "esp_ota_ops.h"
-#ifdef CONFIG_MDNS_ENABLED
 #include "mdns.h"
-#endif
-#ifdef CONFIG_SNTP_ENABLED
-//#include "app_sntp.h"
-#endif
+#include "app_sntp.h"
 
 #define LOG_LOCAL_LEVEL ESP_LOG_INFO
 #include "esp_log.h"
 
 #define HASH_LEN 32 /* SHA-256 digest length */
 static const char *TAG = "application_main";
+EventGroupHandle_t event_group;
+
+static void print_sha256 (const uint8_t *image_hash, const char *label)
+{
+    char hash_print[HASH_LEN * 2 + 1];
+    hash_print[HASH_LEN * 2] = 0;
+    for (int i = 0; i < HASH_LEN; ++i)
+    {
+        sprintf(&hash_print[i * 2], "%02x", image_hash[i]);
+    }
+
+    ESP_LOGI(TAG, "%s: %s", label, hash_print);
+}
+
+void app_shutdown()
+{
+  app_settings_shutdown();
+  app_wifi_shutdown();
+}
+
+static const char *humanSize(uint64_t bytes)
+{
+	char *suffix[] = {"B", "KB", "MB", "GB", "TB"};
+	char length = sizeof(suffix) / sizeof(suffix[0]);
+
+	int i = 0;
+	double dblBytes = bytes;
+
+	if (bytes > 1024)
+   {
+		for (i = 0; (bytes / 1024) > 0 && i<length-1; i++, bytes /= 1024)
+			dblBytes = bytes / 1024.0;
+	}
+
+	static char output[200];
+	sprintf(output, "%.02lf %s", dblBytes, suffix[i]);
+	return output;
+}
 
 void app_main()
 {
-
+  app_settings_startup();
+  app_wifi_startup();
 }
---
+++ main/include/app_settings.h
@@ -0,0 +1,35 @@
+#ifndef APP_SETTINGS_H_
+#define APP_SETTINGS_H_
+
+#include "lwip/ip4_addr.h"
+
+#define LEN_WIFI_SSID     32
+#define LEN_WIFI_PASSWORD 64
+#define LEN_HOSTNAME      32
+#define LEN_MDNS_INSTANCE 32
+#define LEN_NTP_SERVER    32
+#define LEN_TIMEZONE      32
+
+struct app_settings_t
+{
+  int size;
+  char wifi_ssid[LEN_WIFI_SSID];
+  char wifi_password[LEN_WIFI_PASSWORD];
+  char hostname[LEN_HOSTNAME];
+  char mdns_instance[LEN_MDNS_INSTANCE];
+  char ntp_server[LEN_NTP_SERVER];
+  char timezone[LEN_TIMEZONE];
+  bool dhcp;
+  ip4_addr_t ip;
+  ip4_addr_t netmask;
+  ip4_addr_t gateway;
+  ip4_addr_t dns1;
+  ip4_addr_t dns2;
+} settings;
+
+void app_settings_save();
+void app_settings_reset();
+void app_settings_startup();
+void app_settings_shutdown();
+
+#endif
